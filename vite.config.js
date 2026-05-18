8838a2482024cec5ca1f8d7006e24d1ba76157ae
LeonardoFlammini <leonardo98flammini@gmail.com>
2026-05-18 22:17:43 +0200

    feat: add GitHub Actions workflow for deployment to GitHub Pages; update package.json with deploy script and set base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CascinaDellaVachina/',
   plugins: [vue()],
   resolve: {
     alias: {
